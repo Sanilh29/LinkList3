3d3364b4c83c82e907f73513b951ae10ef1fc13e
U-BSD\456570 <456570@SHS-10D8A6AA9.beaverton.k12.or.us>
2017-01-20 14:30:06 -0800

    worked on add

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 
 using namespace std;
 
-void addStudent();
-void deleteStudent();
-void printStudent();
-void averageGPA();
+void addStudent(Node* &h);
+void deleteStudent(Node* &h);
+void printStudent(Node* &h);
+void averageGPA(Node* &h);
 
 int main() {
   char command[10];
@@ -16,17 +16,18 @@ int main() {
   while (playing == true){
     cout << "Enter one of the commands listed: add, delete, print, average, or quit." << endl;
     cin >> command;
+    Node* head = NULL;
     if (0== strcmp(command, "add")){
-
+      addStudent(Node* &h);
     }
     if (0== strcmp(command, "delete")){
-
+      deleteStudent(Node* &h);
     }
     if (0== strcmp(command, "print")){
-
+      printStudent(Node* &h);
     }
     if (0== strcmp(command, "average")){
-
+      averageGPA(Node* &h);
     }
     if (0== strcmp(command, "quit")){
       playing = false;
@@ -34,19 +35,23 @@ int main() {
   }
 }
 
-void addStudent(){
+void addStudent(Node* &h){
+  Node* n = new Node;
+  Student* s = new Student;
+  n.setNext(head);
+  n.setStudent(s);
+  head = n;
+}
+
+void deleteStudent(Node* &h){
 
 }
 
-void deleteStudent(){
+void printStudent(Node* &h){
 
 }
 
-void printStudent(){
-
-}
-
-void averageGPA(){
+void averageGPA(Node* &h){
 
 }
 
